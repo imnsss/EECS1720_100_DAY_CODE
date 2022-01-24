9c8fe17d86aec83e9c3dd6c7d4ef9ff9626397f6
imnsss <zhouyizhi1030@163.com>
2022-01-23 21:37:37 -0500

    1

---
+++ Jan_23/myextension/content.js
@@ -0,0 +1,8 @@
+//alert('Alert!!!')
+chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
+   // alert(request)
+   const re = new RegExp('bear','gi')
+   const matches = document.documentElement.innerHTML.match(re)
+   sendResponse({count: matches.length})
+
+})

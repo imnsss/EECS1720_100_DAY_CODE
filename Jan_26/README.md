f675f6c145c80481bfbb95101f55c04591d7ea04
imnsss <zhouyizhi1030@163.com>
2022-01-26 23:55:12 -0500

    1

---
+++ Jan_26/README.md
@@ -0,0 +1,3 @@
+# DAY12
+- Today, I reviewed html (structure, code etc)
+- I made an html webpage regarding self introduce!
---
+++ Jan_26/ct.js
@@ -0,0 +1,11 @@
+
+function add() {
+
+   var w = document.getElementById("num1").value;
+   var h = document.getElementById("num2").value;
+  
+   var s = w + h;
+
+   document.getElementById("output").innerHTML = "sum: " + s;
+
+}
